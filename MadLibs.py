71b9ca7ab233a434891a1c0d008c2ca54b45051a
sherry.dingman <31575119+SherryDingman@users.noreply.github.com>
2017-09-07 16:04:50 -0400

    Solution to Mad Libs exercise.

---
+++ MadLibs.py
@@ -0,0 +1,12 @@
+# my MadLib program
+# Timothy Anebakem
+# 9/7/17
+def madlib():
+    noun = ("pillow") 
+    verb = ("whirl")
+    adjective = ("fancy")
+    location = ("stadium")
+
+    print ("Take your" ,adjective, noun, "and",verb,"it at the",location,".")
+    
+madlib()
